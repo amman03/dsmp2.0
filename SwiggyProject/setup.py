f55a6a3b9dc169448292b2e24dcbc114b7179ea9
Amman Ahmed <amman.scorpion03@gmail.com>
2026-01-31 23:39:24 +0530

    Adding Templates

---
+++ SwiggyProject/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This Project will help in reccomendation on top 3 food items on Swiggy Home page',
+    author='Amman Ahmed',
+    license='',
+)
